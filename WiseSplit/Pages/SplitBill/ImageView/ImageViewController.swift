76f7a4d8bb2c1e68f9204e512f4896496f7ea694
Beni Garcia <benigarcia853@gmail.com>
2024-06-06 21:28:02 +0700

    Revert "Revert "flow OCR""
    This reverts commit c77c3f36601662e0fe333a67295d316af89b050f.

--- WiseSplit/Pages/SplitBill/ImageView/ImageViewController.swift
+++ WiseSplit/Pages/SplitBill/ImageView/ImageViewController.swift
@@ -1,6 +1,7 @@
 import UIKit
 
 class ImageViewController: UIViewController {
+    let ocrVM = OCRViewModel()
     
     var capturedImage: UIImage?
     var imageFileName: String?
@@ -25,25 +26,73 @@ class ImageViewController: UIViewController {
             imageView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
         
-        
         let button = UIButton(type: .system)
-        button.setTitle("Perform OCR", for: .normal)
-        button.addTarget(self, action: #selector(goToOCRViewController), for: .touchUpInside)
+        button.setTitle("Perform Split Bill", for: .normal)
+        button.addTarget(self, action: #selector(proceedSeparate), for: .touchUpInside)
         button.isHidden = performOCR ?? false ? false : true
         view.addSubview(button)
         button.translatesAutoresizingMaskIntoConstraints = false
+        
         NSLayoutConstraint.activate([
             button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             button.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20)
         ])
-    }
-    
-    @objc func goToOCRViewController() {
-        let ocrViewController = OCRViewController()
-        ocrViewController.capturedImage = capturedImage
-        ocrViewController.imageFileName = imageFileName
         
-        navigationController?.pushViewController(ocrViewController, animated: true)
+        ocrVM.capturedImage = capturedImage
+        ocrVM.performOCR = performOCR ?? false
+        
+        ocrVM.onOCRSuccess = { [weak self] in
+            self?.navigateToSeparateBill()
+        }
+        
+        ocrVM.onOCRError = { error in
+            print(error)
+        }
     }
     
+    @objc func proceedSeparate() {
+        addLoading(onView: self.view)
+        ocrVM.performOCRRequest()
+    }
+    
+    func navigateToSeparateBill() {
+        let separateBillVC = EditBillViewController()
+        separateBillVC.capturedImage = capturedImage
+        
+        let ocrResult = ocrVM.ocrResult
+        
+        for (index, price) in ocrResult.prices.enumerated() {
+            if price.hasPrefix("S") || price.hasPrefix("$") {
+                let modifiedPrice = String(price.dropFirst())
+                
+                if let doublePrice = Double(modifiedPrice) {
+                    let multipliedPrice = doublePrice * 16000
+                    let integerPrice = Int(multipliedPrice)
+                    ocrVM.ocrResult.prices[index] = "\(integerPrice)"
+                    passingItemsToEditSplitBill(separateBillVC: separateBillVC)
+                } else {
+                    print("Invalid price: \(price)")
+                }
+            } else {
+                if let doublePrice = Double(price) {
+                    let multipliedPrice = doublePrice * 16000
+                    let integerPrice = Int(multipliedPrice)
+                    ocrVM.ocrResult.prices[index] = "\(integerPrice)"
+                    passingItemsToEditSplitBill(separateBillVC: separateBillVC)
+                } else {
+                    print("Invalid price: \(price)")
+                }
+            }
+        }
+        
+        navigationController?.pushViewController(separateBillVC, animated: true)
+        removeLoading()
+    }
+    
+    func passingItemsToEditSplitBill(separateBillVC: EditBillViewController) {
+        let ocrResult = ocrVM.ocrResult
+        separateBillVC.itemNames = ocrResult.itemNames
+        separateBillVC.quantities = ocrResult.quantities
+        separateBillVC.prices = ocrResult.prices
+    }
 }
